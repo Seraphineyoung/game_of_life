b3f48a9f0c66f4481e4b6fef678aa853d533468b
Seraphine Young <seraphine_ene@yahoo.com>
2019-05-19 15:47:32 +0100

    created the random state function

--- gameoflife.py
+++ gameoflife.py
@@ -1,7 +1,24 @@
 import pprint
+import random
 
-def dead_state(w,h):
-    return [[0 for i in range(w)] for j in range(h)]
+def dead_state(width,height):
+    return [0 for i in range(width) for j in range(height)]
 
-pprint.pprint(dead_state(5,5))
-    
+# pprint.pprint(dead_state(5,5))
+
+def random_state(width,height):
+    randnum = []
+    state = dead_state(width,height)
+    for i in state:
+        rand_num = random.random()
+        if rand_num >= 0.5:
+            randnum.append(0)
+        else:
+            randnum.append(1)
+    return randnum
+
+
+
+
+pprint.pprint(random_state(5,5))  
+
