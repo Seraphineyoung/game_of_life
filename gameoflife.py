50fd5a0c1807370aa28f743d5c8a665975c923a0
Seraphine Young <seraphine_ene@yahoo.com>
2019-05-19 17:46:30 +0100

    Added render function

--- gameoflife.py
+++ gameoflife.py
@@ -1,24 +1,40 @@
 import pprint
 import random
 
-def dead_state(width,height):
-    return [0 for i in range(width) for j in range(height)]
 
+def dead_state(width,height):
+    return [[0 for i in range(width)] for j in range(height)]
 # pprint.pprint(dead_state(5,5))
 
 def random_state(width,height):
-    randnum = []
     state = dead_state(width,height)
-    for i in state:
-        rand_num = random.random()
-        if rand_num >= 0.5:
-            randnum.append(0)
-        else:
-            randnum.append(1)
-    return randnum
+    for i in range(width):
+        for y in range (height):
+            random_num = random.random()
+            if random_num > 0.65:
+                cell_state = 1
+            else:
+                cell_state = 0
+            state[i][y] = cell_state
+    return state
 
 
+def render(state):
+    display_as = {
+        0: "#",
+        1: u"\u2588"
+    }
+    lines = []
+    
+    for y in range(len(state)):
+        line = '          '
+        for x in range(len(state)):
+          line += display_as[state[x][y]] * 2
+        lines.append(line)
+    print "\n".join(lines)
 
-
-pprint.pprint(random_state(5,5))  
+# pprint.pprint(random_state(5,5))  
+a_dead_state = dead_state(10,10)
+a_random_state = random_state(10,10)
+render(a_random_state)
 
