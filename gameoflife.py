9038460850094cfb9282c1bce811ff6a9966ca45
Seraphine Young <seraphine_ene@yahoo.com>
2019-05-19 18:01:46 +0100

    understanding render function

--- gameoflife.py
+++ gameoflife.py
@@ -5,11 +5,19 @@ import random
 def dead_state(width,height):
     return [[0 for i in range(width)] for j in range(height)]
 # pprint.pprint(dead_state(5,5))
+def state_width(state):
+    return len(state)
+
+def state_height(state):
+    return len(state[0])
+
+
 
 def random_state(width,height):
     state = dead_state(width,height)
-    for i in range(width):
-        for y in range (height):
+
+    for i in range(0,state_width(state)):
+        for y in range (0,state_height(state)):
             random_num = random.random()
             if random_num > 0.65:
                 cell_state = 1
@@ -25,16 +33,15 @@ def render(state):
         1: u"\u2588"
     }
     lines = []
-    
-    for y in range(len(state)):
+    for y in range(state_height(state)):
+        #state here is 20
         line = '          '
-        for x in range(len(state)):
+        for x in range(state_width(state)):
           line += display_as[state[x][y]] * 2
         lines.append(line)
     print "\n".join(lines)
-
-# pprint.pprint(random_state(5,5))  
-a_dead_state = dead_state(10,10)
-a_random_state = random_state(10,10)
+  
+# a_dead_state = dead_state(10,10)
+a_random_state = random_state(20,30)
 render(a_random_state)
 
